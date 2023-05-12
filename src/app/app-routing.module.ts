002974526bde6fbb8c20807be71b3d6d66906426
Vsevolod Pushin <pushin_95@mail.ru>
2023-05-12 18:36:27 +0400

    fixed routing path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import {LayoutComponent} from "./views/layout/layout.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'main',
     component: LayoutComponent,
     children: [
-      {path: 'main', loadChildren: () => import ('./views/home/home.module').then(m => m.HomeModule)},
+      {path: '', loadChildren: () => import ('./views/home/home.module').then(m => m.HomeModule)},
       {path: 'catalog', loadChildren: () => import ('./views/products/products.module').then(m => m.ProductsModule)},
       {path: 'order', loadChildren: () => import ('./views/order/order.module').then(m => m.OrderModule)},
     ]
   },
+  {path: '**', redirectTo: 'main'},
 ];
 
 @NgModule({
