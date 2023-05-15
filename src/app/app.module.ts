ac175785ea12adc238aa38887d41a5ca9789428c
Vsevolod Pushin <pushin_95@mail.ru>
2023-05-15 15:52:11 +0400

    added localization for price display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import {NgModule} from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {HttpClientModule} from "@angular/common/http";
-import {CommonModule} from "@angular/common";
+import {CommonModule, registerLocaleData} from "@angular/common";
 import {SharedModule} from "./shared/shared.module";
 import {LayoutComponent} from "./views/layout/layout.component";
 import {ProductService} from "./shared/services/product.service";
+import localeRu from "@angular/common/locales/ru";
+
+registerLocaleData(localeRu, "ru");
 
 @NgModule({
   declarations: [
@@ -20,7 +23,7 @@ import {ProductService} from "./shared/services/product.service";
     HttpClientModule,
     SharedModule,
   ],
-  providers: [ProductService],
+  providers: [ProductService, {provide: LOCALE_ID, useValue: 'ru'}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
